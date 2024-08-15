4fb4c22e68c7c58964bbdc191cc2b469925ddb01
MarshMiloMoon <milomoon06@gmail.com>
2024-08-15 00:56:46 -0700

    initial commit. we're getting started!!!

---
+++ src/main/java/net/starglobe/mythsmonoliths/MythsAndMonoliths.java
@@ -0,0 +1,18 @@
+package net.starglobe.mythsmonoliths;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class MythsAndMonoliths implements ModInitializer
+{
+	public static final String MOD_ID = "mythsmonoliths";
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize()
+	{
+
+	}
+}
---
+++ src/main/java/net/starglobe/mythsmonoliths/MythsAndMonolithsClient.java
@@ -0,0 +1,12 @@
+package net.starglobe.mythsmonoliths;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class MythsAndMonolithsClient implements ClientModInitializer
+{
+    @Override
+    public void onInitializeClient()
+    {
+
+    }
+}
