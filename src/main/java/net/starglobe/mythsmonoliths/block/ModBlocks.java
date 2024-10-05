8cce7a747f76eae692f710f2dbfdbb9eda2e6820
MarshMiloMoon <milomoon06@gmail.com>
2024-10-05 16:36:07 -0700

    initialized and translated new slabs, stairs, and walls

--- src/main/java/net/starglobe/mythsmonoliths/block/ModBlocks.java
+++ src/main/java/net/starglobe/mythsmonoliths/block/ModBlocks.java
@@ -15,13 +15,19 @@ public class ModBlocks
     public static final Block CALCITE_SLAB = registerBlock("calcite_slab",
             new SlabBlock(AbstractBlock.Settings.copy(Blocks.CALCITE)));
     public static final Block CALCITE_STAIRS = registerBlock("calcite_stairs",
-            new StairsBlock();
+            new StairsBlock(Blocks.CALCITE.getDefaultState(), AbstractBlock.Settings.copy(Blocks.CALCITE)));
     public static final Block CALCITE_WALL = registerBlock("calcite_wall",
             new WallBlock(AbstractBlock.Settings.copy(Blocks.CALCITE)));
     public static final Block POLISHED_CALCITE = registerBlock("polished_calcite",
             new Block(AbstractBlock.Settings.copy(Blocks.CALCITE)));
     public static final Block CALCITE_BRICKS = registerBlock("calcite_bricks",
             new Block(AbstractBlock.Settings.copy(Blocks.CALCITE)));
+    public static final Block CALCITE_BRICK_SLAB = registerBlock("calcite_brick_slab",
+            new SlabBlock(AbstractBlock.Settings.copy(Blocks.CALCITE)));
+    public static final Block CALCITE_BRICK_STAIRS = registerBlock("calcite_brick_stairs",
+            new StairsBlock(Blocks.CALCITE.getDefaultState(), AbstractBlock.Settings.copy(Blocks.CALCITE)));
+    public static final Block CALCITE_BRICK_WALL = registerBlock("calcite_brick_wall",
+            new WallBlock(AbstractBlock.Settings.copy(Blocks.CALCITE)));
     public static final Block CHISELED_CALCITE = registerBlock("chiseled_calcite",
             new Block(AbstractBlock.Settings.copy(Blocks.CALCITE)));
     public static final Block CHISELED_CALCITE_BRICKS = registerBlock("chiseled_calcite_bricks",
@@ -29,19 +35,43 @@ public class ModBlocks
 
     public static final Block SMOOTH_BASALT_SLAB = registerBlock("smooth_basalt_slab",
             new SlabBlock(AbstractBlock.Settings.copy(Blocks.SMOOTH_BASALT)));
+    public static final Block SMOOTH_BASALT_STAIRS = registerBlock("smooth_basalt_stairs",
+            new StairsBlock(Blocks.SMOOTH_BASALT.getDefaultState(), AbstractBlock.Settings.copy(Blocks.SMOOTH_BASALT)));
+    public static final Block SMOOTH_BASALT_WALL = registerBlock("smooth_basalt_wall",
+            new WallBlock(AbstractBlock.Settings.copy(Blocks.SMOOTH_BASALT)));
     public static final Block POLISHED_SMOOTH_BASALT = registerBlock("polished_smooth_basalt",
             new Block(AbstractBlock.Settings.copy(Blocks.SMOOTH_BASALT)));
     public static final Block BASALT_BRICKS = registerBlock("basalt_bricks",
             new Block(AbstractBlock.Settings.copy(Blocks.SMOOTH_BASALT)));
+    public static final Block BASALT_BRICK_SLAB = registerBlock("basalt_brick_slab",
+            new SlabBlock(AbstractBlock.Settings.copy(Blocks.SMOOTH_BASALT)));
+    public static final Block BASALT_BRICK_STAIRS = registerBlock("basalt_brick_stairs",
+            new StairsBlock(Blocks.SMOOTH_BASALT.getDefaultState(), AbstractBlock.Settings.copy(Blocks.SMOOTH_BASALT)));
+    public static final Block BASALT__BRICK_WALL = registerBlock("basalt_brick_wall",
+            new WallBlock(AbstractBlock.Settings.copy(Blocks.SMOOTH_BASALT)));
     public static final Block CHISELED_BASALT = registerBlock("chiseled_basalt",
             new Block(AbstractBlock.Settings.copy(Blocks.SMOOTH_BASALT)));
 
     public static final Block SMOOTH_NETHER_BRICKS = registerBlock("smooth_nether_bricks",
             new Block(AbstractBlock.Settings.copy(Blocks.NETHER_BRICKS)));
+    public static final Block SMOOTH_NETHER_BRICK_SLAB = registerBlock("smooth_nether_brick_slab",
+            new SlabBlock(AbstractBlock.Settings.copy(Blocks.NETHER_BRICKS)));
+    public static final Block SMOOTH_NETHER_BRICK_STAIRS = registerBlock("smooth_nether_brick_stairs",
+            new StairsBlock(Blocks.NETHER_BRICKS.getDefaultState(), AbstractBlock.Settings.copy(Blocks.NETHER_BRICKS)));
+    public static final Block SMOOTH_NETHER_BRICK_WALL = registerBlock("smooth_nether_brick_wall",
+            new WallBlock(AbstractBlock.Settings.copy(Blocks.NETHER_BRICKS)));
     public static final Block SMOOTH_RED_NETHER_BRICKS = registerBlock("smooth_red_nether_bricks",
             new Block(AbstractBlock.Settings.copy(Blocks.RED_NETHER_BRICKS)));
+    public static final Block SMOOTH_RED_NETHER_BRICK_SLAB = registerBlock("smooth_nether_brick_slab",
+            new SlabBlock(AbstractBlock.Settings.copy(Blocks.RED_NETHER_BRICKS)));
+    public static final Block SMOOTH_RED_NETHER_BRICK_STAIRS = registerBlock("smooth_nether_brick_stairs",
+            new StairsBlock(Blocks.RED_NETHER_BRICKS.getDefaultState(), AbstractBlock.Settings.copy(Blocks.RED_NETHER_BRICKS)));
+    public static final Block SMOOTH_RED_NETHER_BRICK_WALL = registerBlock("smooth_nether_brick_wall",
+            new WallBlock(AbstractBlock.Settings.copy(Blocks.RED_NETHER_BRICKS)));
     public static final Block CHISELED_RED_NETHER_BRICKS = registerBlock("chiseled_red_nether_bricks",
             new Block(AbstractBlock.Settings.copy(Blocks.RED_NETHER_BRICKS)));
+    public static final Block RED_NETHER_BRICK_FENCE = registerBlock("red_nether_brick_fence",
+            new FenceBlock(AbstractBlock.Settings.copy(Blocks.NETHER_BRICK_FENCE)));
 
     public static final Block BEATEN_COPPER = registerBlock("beaten_copper",
             new Block(AbstractBlock.Settings.copy(Blocks.COPPER_BLOCK)));
@@ -115,18 +145,35 @@ public class ModBlocks
         MythsAndMonoliths.LOGGER.info("Registering Mod Blocks for " + MythsAndMonoliths.MOD_ID);
 
         ItemGroupEvents.modifyEntriesEvent(ItemGroups.BUILDING_BLOCKS).register(entries -> {
+            entries.add(CALCITE_SLAB);
+            entries.add(CALCITE_STAIRS);
+            entries.add(CALCITE_WALL);
             entries.add(POLISHED_CALCITE);
             entries.add(CALCITE_BRICKS);
+            entries.add(CALCITE_BRICK_SLAB);
+            entries.add(CALCITE_BRICK_STAIRS);
+            entries.add(CALCITE_BRICK_WALL);
             entries.add(CHISELED_CALCITE);
             entries.add(CHISELED_CALCITE_BRICKS);
+            entries.add(SMOOTH_BASALT_SLAB);
+            entries.add(SMOOTH_BASALT_STAIRS);
+            entries.add(SMOOTH_BASALT_WALL);
             entries.add(POLISHED_SMOOTH_BASALT);
             entries.add(BASALT_BRICKS);
+            entries.add(BASALT_BRICK_SLAB);
+            entries.add(BASALT_BRICK_STAIRS);
+            entries.add(BASALT__BRICK_WALL);
             entries.add(CHISELED_BASALT);
-            entries.add(POLISHED_CALCITE);
             entries.add(SMOOTH_NETHER_BRICKS);
+            entries.add(SMOOTH_NETHER_BRICK_SLAB);
+            entries.add(SMOOTH_NETHER_BRICK_STAIRS);
+            entries.add(SMOOTH_NETHER_BRICK_WALL);
             entries.add(SMOOTH_RED_NETHER_BRICKS);
+            entries.add(SMOOTH_RED_NETHER_BRICK_SLAB);
+            entries.add(SMOOTH_RED_NETHER_BRICK_STAIRS);
+            entries.add(SMOOTH_RED_NETHER_BRICK_WALL);
             entries.add(CHISELED_RED_NETHER_BRICKS);
-            entries.add(POLISHED_CALCITE);
+            entries.add(RED_NETHER_BRICK_FENCE);
             entries.add(BEATEN_COPPER);
             entries.add(BEATEN_COPPER_SLAB);
             entries.add(SMOOTH_BEATEN_COPPER);
